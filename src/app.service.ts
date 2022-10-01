4bbd0de4ed4e68222edf1fd5ba00a2805796f62a
Denis Savenok <denissavenok@gmail.com>
2022-10-01 13:22:24 +0300

    Install ConfigModule, check DB_LOGIN

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hebrew songs';
+    return 'Hebrew songs ' + process.env.DB_LOGIN;
   }
 }
