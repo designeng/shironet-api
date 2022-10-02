e7d6164339b6b51398f37f5bb41b3d82f5ce8c4c
Denis Savenok <denissavenok@gmail.com>
2022-10-02 17:13:49 +0300

    Inject wordModel, find all

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WordSchema } from './schemas/word.schema';
 
 require('dotenv').config();
 
@@ -10,7 +11,10 @@ const { DB_LOGIN, DB_PASSWORD, DB_BASE_URL } = process.env;
 const url = `mongodb+srv://${DB_LOGIN}:${DB_PASSWORD}@${DB_BASE_URL}/?retryWrites=true&w=majority`;
 
 @Module({
-  imports: [MongooseModule.forRoot(url)],
+  imports: [
+    MongooseModule.forRoot(url),
+    MongooseModule.forFeature([{ name: 'Word', schema: WordSchema }]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Word } from './interfaces/word.interface';
 
 @Injectable()
 export class AppService {
+  constructor(@InjectModel('Word') private readonly wordModel: Model<Word>) {}
+
   getHello(): string {
     return 'Hebrew songs ' + process.env.DB_LOGIN;
+  }
+
+  findAll() {
+    return this.wordModel.find().exec();
   }
 }
