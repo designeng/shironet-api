043b590f149d13a2b053b9feda3784da2e49d2e0
Denis Savenok <denissavenok@gmail.com>
2022-10-02 15:32:23 +0300

    Use dotenv, remove ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+require('dotenv').config();
+
+const { DB_LOGIN, DB_PASSWORD, DB_BASE_URL } = process.env;
+
+const url = `mongodb+srv://${DB_LOGIN}:${DB_PASSWORD}@${DB_BASE_URL}/?retryWrites=true&w=majority`;
+
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [MongooseModule.forRoot(url)],
   controllers: [AppController],
   providers: [AppService],
 })
